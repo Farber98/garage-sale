4c70658a417862e4e1068a249a6df5ecadf51b5b
Farber98 <juan.farber@smartcontract.com>
2025-05-02 13:22:54 -0300

    add middlware support

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -7,21 +7,25 @@ import (
 	"os"
 )
 
-type Handler func(context.Context, http.ResponseWriter, *http.Request) error
+type HandlerFunc func(context.Context, http.ResponseWriter, *http.Request) error
 
 type App struct {
 	*http.ServeMux
 	shutdown chan os.Signal
+	mw       []MidHandler
 }
 
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...MidHandler) *App {
 	return &App{
 		ServeMux: http.NewServeMux(),
 		shutdown: shutdown,
+		mw:       mw,
 	}
 }
 
-func (app *App) HandleFunc(pattern string, handler Handler) {
+func (app *App) HandleFunc(pattern string, handler HandlerFunc, mw ...MidHandler) {
+	handler = wrapMiddleware(mw, handler)
+	handler = wrapMiddleware(app.mw, handler)
 
 	// Callback func to inject custom logic
 	h := func(w http.ResponseWriter, r *http.Request) {
