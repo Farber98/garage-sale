851d5adef1ce0f3b5f2d43dda984116e46820c4c
Farber98 <juan.farber@smartcontract.com>
2025-05-03 19:18:59 -0300

    logging midd

--- api/services/sales/mux/mux.go
+++ api/services/sales/mux/mux.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/Farber98/garage-sale/api/services/sales/route/sys/checkapi"
+	"github.com/Farber98/garage-sale/app/mid"
+	"github.com/Farber98/garage-sale/foundation/logger"
 	"github.com/Farber98/garage-sale/foundation/web"
 )
 
 // WebAPI constructs a http.Handler with all app routes bound
-func WebAPI(shutdown chan os.Signal) *web.App {
-	app := web.NewApp(shutdown)
+func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
+	app := web.NewApp(shutdown, mid.Logger(log))
 
 	checkapi.Routes(app)
 
---
+++ app/mid/logger.go
@@ -0,0 +1,25 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Farber98/garage-sale/foundation/logger"
+	"github.com/Farber98/garage-sale/foundation/web"
+)
+
+func Logger(log *logger.Logger) web.MidHandler {
+	m := func(handler web.HandlerFunc) web.HandlerFunc {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			log.Info(ctx, "request started", "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
+
+			err := handler(ctx, w, r)
+
+			log.Info(ctx, "request completed", "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
+
+			return err
+		}
+		return h
+	}
+	return m
+}
